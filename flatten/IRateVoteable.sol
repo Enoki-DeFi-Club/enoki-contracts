51ec3a28a265c1246e661a67ba1c217a014acd80
chef-wisdom <chef-wisdom@pm.me>
2021-01-11 14:04:02 -0600

    remove old flattened contracts

--- flatten/IRateVoteable.sol
+++
@@ -1,9 +0,0 @@
-// Root file: contracts/interfaces/IRateVoteable.sol
-
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.0;
-
-interface IRateVoteable {
-    function changeRate(uint256 percentage) external;
-}
